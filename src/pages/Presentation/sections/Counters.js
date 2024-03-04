76e0ac156a0abbd6dc48d5ca3b476e137bc023eb
Arafat <arafat.smart.software@gmail.com>
2024-03-04 18:30:56 +0600

    first

---
+++ src/pages/Presentation/sections/Counters.js
@@ -0,0 +1,64 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+import DefaultCounterCard from "examples/Cards/CounterCards/DefaultCounterCard";
+
+function Counters() {
+  return (
+    <MKBox component="section" py={3}>
+      <Container>
+        <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={25}
+              suffix="+"
+              title="All Projects"
+              description="Tis is my all Production Projects"
+            />
+          </Grid>
+          <Grid item xs={12} md={4} display="flex">
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
+            <DefaultCounterCard
+              count={5}
+              suffix="+"
+              title="Fronend design"
+              description="This is Creative Project Design"
+            />
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={20}
+              suffix="+"
+              title="Application"
+              description="My Best workSide in Web Applications"
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Counters;
